e9412bfaf83cbbde99c325daaeff0325f7e143e0
Michael Irvine <michael.j.irvine@gmail.com>
2022-12-22 16:03:15 -0500

    fixes issues

--- macros/setup.sql
+++ macros/setup.sql
@@ -1,5 +1,5 @@
 {% macro setup(
-        api_path = 'https://7f7si8f6k6.execute-api.us-east-1.amazonaws.com/query'
+        api_path = 'https://7f7si8f6k6.execute-api.us-east-1.amazonaws.com/query',
         api_key = none,
         client_id = none,
         iam_role = none,
@@ -9,7 +9,7 @@
         {{ exceptions.raise_compiler_error("API key, Client ID, and IAM role must be provided. Please reach out to Michael Irvine on the dbt Slack or at michael.j.irvine@gmail.com to get your keys.") }}
     {% endif %}
 
-    {{ return(adapter.dispatch('setup', 'natural_language')(api_key, client_id, iam_role, dev)) }}
+    {{ return(adapter.dispatch('setup', 'natural_language')(api_path, api_key, client_id, iam_role, dev)) }}
 {% endmacro %}
 
 {% macro default__setup() %}
